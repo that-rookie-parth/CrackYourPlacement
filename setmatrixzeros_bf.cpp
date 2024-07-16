4f6a85a8e4e43531a2048f289b597758f8ffa7a8
that-rookie-parth <kulshreshthaparth@gmail.com>
2024-07-16 23:08:03 +0530

    updated the bf sol

--- setmatrixzeros_bf.cpp
+++ setmatrixzeros_bf.cpp
@@ -32,6 +32,15 @@ public:
                 }
             }
         }
+        for (int i = 0; i < matrix.size(); i++)
+        {
+            for (int j = 0; j < matrix[0].size(); j++)
+            {
+                if(matrix[i][j]==-1){
+                    matrix[i][j]=0;
+                }
+            }
+        }
         return matrix;
     }
 };
